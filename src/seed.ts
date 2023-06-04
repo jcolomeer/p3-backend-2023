2ede83152a32d0133fc861c48ca20d9f887dc52c
jcolomeer <jococi99@gmail.com>
2023-06-04 23:09:33 +0200

    modified seed to better test the endpoints

--- src/seed.ts
+++ src/seed.ts
@@ -3,12 +3,12 @@ const prisma = new PrismaClient();
 
 const newFilms = [];
 const films = [
-  { title: "Lady Bird", year: 2017 },
-  { title: "La La Land", year: 2016 },
-  { title: "Knives Out", year: 2019 },
-  { title: "Decision to Leave", year: 2022 },
-  { title: "Marriage Story", year: 2019 },
-  { title: "Princess Mononoke", year: 1996 },
+  { title: "Lady Bird", year: 2017, id: "film1" },
+  { title: "La La Land", year: 2016, id: "film2" },
+  { title: "Knives Out", year: 2019, id: "film3" },
+  { title: "Decision to Leave", year: 2022, id: "film4" },
+  { title: "Marriage Story", year: 2019, id: "film5" },
+  { title: "Princess Mononoke", year: 1996, id: "film6" },
 ];
 
 for (let element of films) {
@@ -22,7 +22,8 @@ for (let element of films) {
 const newUser = await prisma.user.create({
   data: {
     email: "prueba@email.com",
-    username: "user1",
+    username: "myusr",
+    id: "user1",
   },
 });
 
@@ -33,16 +34,19 @@ const newReviews = await prisma.review.createMany({
       rating: 4.5,
       description: "Amazing",
       userId: newUser.id,
+      id: "review1",
     },
     {
       filmId: newFilms[2].id,
       rating: 4,
       userId: newUser.id,
+      id: "review2",
     },
     {
       filmId: newFilms[3].id,
       rating: 5,
       userId: newUser.id,
+      id: "review3",
     },
   ],
 });
